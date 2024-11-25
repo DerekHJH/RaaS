065c1f1c95fee33249a587f022f944617c4fc6d8
Junhao Hu <derekhjh9@gmail.com>
2024-11-25 15:37:26 +0800

    Try precommit

--- benchmarks/eval_engines/e2e/run_all.py
+++ benchmarks/eval_engines/e2e/run_all.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for dataset in ['math500']:
+for dataset in ['needle', 'math500']:
     for model in ['peiyi9979/mistral-7b-sft']:
         for approach in ['full', 'quest']:
             command = f'python3 main.py --dataset {dataset} --model {model} --approach {approach}'
