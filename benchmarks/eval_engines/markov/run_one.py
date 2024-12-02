2e084e5f8183432cce52315d7d326c6ad4b3e0d9
Junhao Hu <derekhjh9@gmail.com>
2024-12-02 11:36:51 +0800

    Start Markov

--- benchmarks/eval_engines/markov/run_one.py
+++ benchmarks/eval_engines/markov/run_one.py
@@ -1,12 +1,16 @@
 import subprocess
 
-# These variables are ususally fixed for Markov Property testing
+# Configure the following three variables as needed
 dataset = "math500"
 model = "peiyi9979/mistral-7b-sft"
 approach = "full"
+num_tot_data = 1
 
 
-command = f"python3 main.py --dataset {dataset} --model {model} --approach {approach}"
+command = (
+    f"python3 main.py --dataset {dataset} --model {model}"
+    f" --approach {approach} --num_tot_data {num_tot_data}"
+)
 print(f"Running command: {command}")
 process = subprocess.Popen(command, shell=True)
 process.wait()
