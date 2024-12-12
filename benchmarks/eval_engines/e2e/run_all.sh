32096f256cc0ad031123ba7aca9169b588e4febe
Junhao Hu <derekhjh9@gmail.com>
2024-12-12 15:41:11 +0800

    Finish adapting two models from openr on MATH500

--- benchmarks/eval_engines/e2e/run_all.sh
+++ benchmarks/eval_engines/e2e/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for dataset in "math500"; do
-    for model in "peiyi9979/mistral-7b-sft"; do
+    for model in "peiyi9979/mistral-7b-sft" "Qwen/Qwen2.5-Math-7B-Instruct"; do
         for approach in "full" "streamingllm"; do
             command="python3 main.py --dataset ${dataset} --model ${model} --approach ${approach}"
             echo "Running command: ${command}"
--- benchmarks/eval_engines/e2e/run_one.sh
+++ benchmarks/eval_engines/e2e/run_one.sh
@@ -1,7 +1,9 @@
 # Configure the following three variables as needed
 dataset="math500"
-model="AIDC-AI/Marco-o1"
-approach="streamingllm"
+# model="AIDC-AI/Marco-o1"
+# model="peiyi9979/mistral-7b-sft"
+model="Qwen/Qwen2.5-Math-7B-Instruct"
+approach="full"
 
 command="python3 main.py --dataset ${dataset} --model ${model} --approach ${approach}"
 echo "Running command: ${command}"
